487950dc3429879bfb5c3bf2c71f7a813fdba291
rene.vucko <rene.vucko@comtradegaming.com>
2025-04-08 20:36:30 +0200

    logging

--- src/game/objects/ui/Score.ts
+++ src/game/objects/ui/Score.ts
@@ -24,6 +24,7 @@ export class Score extends Phaser.GameObjects.Text {
     this.setOrigin(0.5);
 
     this.#eventBusComponent.on(CUSTOM_EVENTS.ENEMY_DESTROYED, (enemy: any) => {
+      console.log(ENEMY_SCORES[enemy.constructor.name], 'score', ENEMY_SCORES[enemy.constructor.name].toString(10));
       this.#score += ENEMY_SCORES[enemy.constructor.name];
       this.setText(this.#score.toString(10));
     });
